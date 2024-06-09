82c40f1fcb110a7a2dee5d3e01371349c263e5f0
Gustavo Trigolo <gustavotrigolo8@gmail.com>
2024-06-09 15:29:26 +0000

    style: criação dos estilos globais

---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components"
+
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        paddin: 0;
+        box-sizing: border-box;
+    }
+`
