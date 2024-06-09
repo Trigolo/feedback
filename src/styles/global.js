c29b60b79a5cbbe8867ae79387b8147bd225c9c5
Gustavo Trigolo <gustavotrigolo8@gmail.com>
2024-06-09 21:25:42 +0000

    style:posicionamento do card no centro da tela

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,5 +6,20 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         paddin: 0;
         box-sizing: border-box;
+        font-family: "Overpass", sans-serif;
+    }
+
+    :root{
+        font-size: 62.5%;
+    }
+
+    body{
+        font-size:1.6rem;
+        background: ${props => props.theme.homeBackground};
+
+        height:100svh;
+        display: flex;
+        justify-content: center;
+        aling-items: center;
     }
 `
