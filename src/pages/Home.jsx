f0e21cb396d7433bb167565ad4ee8f1ef0a79551
Gustavo Trigolo <gustavotrigolo8@gmail.com>
2024-06-09 23:31:39 +0000

    style: estilização completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,43 @@
-import { ButtonContainer, CardContanier, IconContainer } from "./Home.styles";
+import { ButtonContainer, CardContanier, IconContainer, ResultContainer } from "./Home.styles";
 
 import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
+  let aparecerResultado = true
+
   return (
-    <CardContanier>
+    aparecerResultado === false ? (
+      <CardContanier>
         <IconContainer>
-            <img src={iconStarImg} alt="icone de estrela" />
+          <img src={iconStarImg} alt="icone de estrela" />
         </IconContainer>
 
         <h1>Como foi o atendimento?</h1>
         <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+
         <ButtonContainer>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
         </ButtonContainer>
 
         <button>Enviar</button>
-    </CardContanier>
+      </CardContanier>
+    ) : (
+      <CardContanier>
+        <img src={thankYouImg} alt="imagem de agradecimento" />
 
+        <ResultContainer>
+          <p>Você selecionou 4 de 5</p>
+        </ResultContainer>
+
+        <h1>Obrigado!</h1>
+
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+      </CardContanier>)
   )
 }
+
